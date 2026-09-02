ecafef3f4a4d2fee56ad1ec2204d84f6f00fcb2f
IU25CA007-AMRITHA <amrithar884@gmail.com>
2026-09-02 11:31:50 +0530

    Update solution.sql

--- Assignment03_Alter_Student/solution.sql
+++ Assignment03_Alter_Student/solution.sql
@@ -1,19 +1,4 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
-CREATE TABLE Student(
-    StudentID INT(5) PRIMARY KEY,
-    StudentName VARCHAR(20) NOT NULL,
-    DOB DATE,
-    Gender VARCHAR(10),
-    DepartmentID INT(5)
-);
-
--- Alter Student table
-
--- Add Email
-
--- Add PhoneNumber
-
--- Display structure
+Create database collegeDB;
+Use collegeDB;
+Alter table student add email varchar(30) ;
+Alter table student add phoneno int(10) ;
