ccd57b298c760c9b38ad9c0d1d10da569224deb8
IU25CA007-AMRITHA <amrithar884@gmail.com>
2026-09-03 09:08:42 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,9 +1,4 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
--- Create Course table
-
--- Insert three records
-
--- Display structure
+Create database collegeDB;
+Use collegeDB;
+Create table course(courseID inter(5), coursename varchar(20), credits int(5), deptID int (5)) ;
+Insert into course(101, 'python programming', 3,203), (102,'Java', 4,203), (103, 'MySql', 2,301), (104, 'Excel', 1,302) ;
