ab7c95be2caee2fd33e4bba9fb640b19adfbd479
IU25CA007-AMRITHA <amrithar884@gmail.com>
2026-09-16 11:03:43 +0530

    Update solution.sql

--- Assignment10_Left_Right_Join/solution.sql
+++ Assignment10_Left_Right_Join/solution.sql
@@ -1,13 +1,8 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
--- Create Course
-
--- Create Enrollment
-
--- Insert sample records
-
--- LEFT JOIN
-
--- RIGHT JOIN
+Create database Amritha;
+Use Amritha;
+CREATE TABLE COURSE(CourseID int(5), CourseName varchar(20), Credits int(3)) ;
+Insert into course values(201, 'datastructure', 3), (202, database system', 4), (203, 'Mathematics', 4) ;
+CREATE TABLE ENROLLMENT (Enrollid int(5), student ID int(5), courseId int(5)) ;
+Insert into enrollment values(1, 1001,201), (2, 1001,202), (3, 1002,203), (4, 1003,201) ;
+SELECT courseName, credits, enrollID, studentID from course C LEFT JOIN enrollment e on c.courseid=e.courseid;
+SELECT courseName, credits, enrollid, studentid from course c RIGHT JOIN enrollment e on c.courseid=e.courseid;
